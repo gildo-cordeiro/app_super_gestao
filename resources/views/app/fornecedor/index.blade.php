7bc70d7df8ce2a6b4ccd3c52f5c987c7db526354
gildo-cordeiro <gildocordeiro2.0@gmail.com>
2020-12-23 17:13:22 -0300

    adicionando alguns controllers, views e rotas.

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,3 @@
+<h3>Fornecedor</h3>
+
+{{-- Comentario que será descartado pelo interpletador blade --}}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Requests\PrincipalController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos';})->name('app.produtos');
 });
+
+
